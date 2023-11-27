7e0b86beeb5c2c1a1007defdb41d24064c2e7370
sdowd20 <sdowd20@berkeley.edu>
2023-11-27 12:12:04 -0500

    Small changes

--- Scripts/Load.SDM.R
+++ Scripts/Load.SDM.R
@@ -28,20 +28,20 @@ world <- ne_countries(scale = "medium", returnclass = "sf")
 
 #Load in datasets, 10 km 
 ##CPUE
-df_CPUE <- read.csv("~/Documents/GitHub/NCBlueCrab_Predators/Data/CPUE/CPUE_grid_avg_edt.11.21.23.csv")
+df_CPUE <- read.csv("~/Desktop/Ch1Data/CPUE/CPUE_grid_avg_edt.11.21.23.csv")
 df_CPUE <- df_CPUE[,-1]
 df_CPUE <- df_CPUE %>% mutate_at(c("Sedsize_common", "ShorelineType", "Survey"), as.factor)
-df_CPUE$Speciescommonname <- gsub(" ", ".", df_CPUE$Speciescommonname)
-# colnames(df_CPUE) <- gsub(pattern = "_", replacement = "", colnames(df_CPUE))
+df_CPUE$Speciescommonname <- gsub(" ", "", df_CPUE$Speciescommonname)
+colnames(df_CPUE) <- gsub(pattern = "_", replacement = "", colnames(df_CPUE))
 
 ##Count dataset 
-df_count <- read_csv("~/Documents/GitHub/NCBlueCrab_Predators/Data/CPUE/CPUE_grid_count_avg_edt.11.21.23.csv")  
+df_count <- read_csv("~/Desktop/Ch1Data/CPUE/CPUE_grid_count_avg_edt.11.21.23.csv")
 df_count <- df_count %>% dplyr::select(-c(...1, CPUE, CPUE_stdzd, mean_CPUE, mean_CPUE_stdzd)) #need to remove or R will get confused 
 df_count$Speciescommonname <- gsub(" ", "", df_count$Speciescommonname)
 colnames(df_count) <- gsub(pattern = "_", replacement = "", colnames(df_count))
 
 ##Binary dataset
-df_binary <- read_csv("~/Documents/GitHub/NCBlueCrab_Predators/Data/CPUE/CPUE_grid_binary_edt.11.22.23.csv")
+df_binary <- read_csv("~/Desktop/Ch1Data/CPUE/CPUE_grid_binary_edt.11.22.23.csv")
 df_binary <- df_binary %>% dplyr::select(-c(...1, CPUE, CPUE_stdzd, mean_CPUE, mean_CPUE_stdzd, avg_count))
 df_binary$Speciescommonname <- gsub(" ", "", df_binary$Speciescommonname)
 colnames(df_binary) <- gsub(pattern = "_", replacement = "", colnames(df_binary))
@@ -58,39 +58,14 @@ df_binary_wide_P915 <- df_binary %>% filter(Survey %in% "P915") %>% ungroup() %>
 
 #Pivot-wider datasets: P915 and P120 
 ##CPUE
-df_CPUE$SpeciesSurvey <- paste(df_CPUE$Speciescommonname, df_CPUE$Survey, sep= ".")
-df_CPUE$mean_CPUE_stdzd <- as.numeric(df_CPUE$mean_CPUE_stdzd)
-df_CPUE_wide_both <- df_CPUE %>% filter(Survey %in% "P120") %>% dplyr::select(-Speciescommonname, -Survey) %>% ungroup() %>% pivot_wider(names_from = "SpeciesSurvey", values_from = "mean_CPUE_stdzd") %>% drop_na()
+df_CPUE$SpeciesSurvey <- paste(df_CPUE$Speciescommonname, df_CPUE$Survey, sep= "")
+df_CPUE$meanCPUEstdzd <- as.numeric(df_CPUE$meanCPUEstdzd)
+df_CPUE_wide_both <- df_CPUE %>% filter(Survey %in% "P120"|Survey %in% "P915") %>% dplyr::select(-Speciescommonname, -Survey) %>% ungroup() %>% pivot_wider(names_from = "SpeciesSurvey", values_from = "meanCPUEstdzd") %>% drop_na()
 
-colnames(df_CPUE)
 ##Count
 df_count$SpeciesSurvey <- paste(df_count$Speciescommonname, df_count$Survey, sep= "")
 df_count_wide_both <- df_count %>% filter(Survey %in% "P915"|Survey %in% "P120") %>% dplyr::select(-Speciescommonname, -Survey) %>% ungroup() %>% pivot_wider(names_from = "SpeciesSurvey", values_from = "avgcount") %>% drop_na()
 
-test <- df_count %>% filter(Survey %in% "P120") %>% ungroup() %>% pivot_wider(names_from = "Speciescommonname", values_from = "avgcount")
 ##Binary 
-
-
-test <- df_count %>% filter(Survey %in% "P120") %>% dplyr::select(-c(...1, CPUE, CPUE_stdzd, mean_CPUE, mean_CPUE_stdzd)) %>% pivot_wider(names_from = "Speciescommonname", values_from = "avg_count")
-
-
-
-CPUE_grid_avg_edt$SpeciesSurvey <- paste(CPUE_grid_avg_edt$Speciescommonname, CPUE_grid_avg_edt$Survey, sep= ".")
-
-CPUE_wide_na <- CPUE_grid_avg_edt %>% filter(Survey %in% "P915"|Survey %in% "P120") %>%  dplyr::select(-Speciescommonname, -Survey) %>% ungroup() %>% pivot_wider(names_from = "SpeciesSurvey", values_from = "mean_CPUE_stdzd") %>% drop_na()
-
-
-#IT'S P120 that is getting messed up!! 
-
-CPUE_grid_avg_edt <- read.csv("~/Documents/GitHub/NCBlueCrab_Predators/Data/CPUE/CPUE_grid_avg_edt.11.21.23.csv")
-CPUE_grid_avg_edt <- CPUE_grid_avg_edt[,-1]
-CPUE_grid_avg_edt <- CPUE_grid_avg_edt %>% mutate_at(c("Sedsize_common", "ShorelineType",  "Survey"), as.factor)
-CPUE_grid_avg_edt$Speciescommonname <- gsub(" ", ".", CPUE_grid_avg_edt$Speciescommonname)
-CPUE_grid_avg_edt$SpeciesSurvey <- paste(CPUE_grid_avg_edt$Speciescommonname, CPUE_grid_avg_edt$Survey, sep= ".")
-
-CPUE_wide_na <- CPUE_grid_avg_edt %>% filter(Survey %in% "P915"|Survey %in% "P195") %>%  dplyr::select(-Speciescommonname, -Survey) %>% ungroup() %>% pivot_wider(names_from = "SpeciesSurvey", values_from = "mean_CPUE_stdzd") %>% drop_na()
-
-
-
-df_CPUE$SpeciesSurvey <- paste(df_CPUE$Speciescommonname, df_CPUE$Survey, sep= "")
-CPUE_wide_na <- df_CPUE %>% filter(Survey %in% "P915") %>%  dplyr::select(-Speciescommonname, -Survey) %>% ungroup() %>% pivot_wider(names_from = "SpeciesSurvey", values_from = "meanCPUEstdzd") %>% drop_na()
+df_binary$SpeciesSurvey <- paste(df_binary$Speciescommonname, df_binary$Survey, sep= "")
+df_binary_wide_both <- df_binary %>% filter(Survey %in% "P915"|Survey %in% "P120") %>% dplyr::select(-Speciescommonname, -Survey) %>% ungroup() %>% pivot_wider(names_from = "SpeciesSurvey", values_from = "binary") %>% drop_na()
